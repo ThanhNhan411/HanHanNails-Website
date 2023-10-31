a628c34db6654845bee7f641454752a6fa762e2a
Thanhnhan411 <thanhnhan41100@gmail.com>
2023-11-01 00:03:26 +0700

    Create base app for website

---
+++ hanhannails/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="container">
+            <a href="" className="header-item">Trang chủ</a>
+            <a href="" className="header-item">Giới thiệu</a>
+            <a href="" className="header-item">Dịch vụ</a>
+            <a href="" className="header-item">Nailsbox</a>
+        </div>
+    );
+}
+
+export default Header;
